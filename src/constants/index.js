b478751e12608a9b65415d5ca2970fb5191797df
Rong Feng Ye <rongfeny@andrew.cmu.edu>
2023-08-02 16:55:46 -0400

    Work experience section

--- src/constants/index.js
+++ src/constants/index.js
@@ -122,7 +122,7 @@ import {
       iconBg: "#383E56",
       date: "March 2020 - April 2021",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Developing aasdasdnd maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
