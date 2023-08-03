180aae044d051ce0eb929eb17b4c0bd20cc81b69
Rong Feng Ye <rongfeny@andrew.cmu.edu>
2023-08-02 23:29:26 -0400

    working ball atm

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -5,13 +5,18 @@ import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/d
 import CanvasLoader from '../Loader';
 
 const Ball = (props) => {
-  // const [logo] = useTexture([props.imgUrl]);
+  const [logo] = useTexture([props.imgUrl])
+  console.log("hi, In the ball function, this is image\n", props.imgUrl, "\n this is logo", logo)
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+
       <ambientLight intensity={1.25} />
+
       <directionalLight position={[0,0,0.05]} />
-      <mesh scale={2.75}>
+
+      <mesh castShadow receiveShadow scale={2.75}>
+
         <icosahedronGeometry args={[1,1]} />
         <meshStandardMaterial 
           color="#fff8eb"
@@ -20,24 +25,22 @@ const Ball = (props) => {
           flatShading
         />
 
-        {/* <Decal 
-          position={[0,0,1]}
-          rotation={[ 2 * Math.PI, 0, 6.25]}
-          flatShading
-          map={useTexture(logo)} 
-        />  */}
+        <Decal position={[0,0,1]} map={logo} />
 
       </mesh>
+
     </Float>
   )
 }
 
 const BallCanvas = ({ icon }) => {
+  // console.log("in the ball canvas function")
   return (
     <Canvas 
       frameLoop="demand"
       gl={{ preserveDrawingBuffer: true }}
     >
+      
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
